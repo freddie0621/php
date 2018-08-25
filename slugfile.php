0ddd0026f330af5abf6ae6e98b6a01dde4aefb5e
freddie0621 <freddiebay@gmail.com>
2018-08-25 17:37:19 +0200

    Create slugfile.php

---
+++ slugfile.php
@@ -0,0 +1,7 @@
+<?php 
+//On formate le nom du fichier ici...
+		 $fichier = strtr($fichier, 
+			  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+			  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+		 $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+?>
